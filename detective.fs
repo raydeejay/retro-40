bb791076ef5ef593e8a3a1545d5e6787c2246a54
Sergi Reyner <sergi.reyner@gmail.com>
2018-08-19 14:19:54 +0100

    Added a little dude to be chased around in detective.fs

--- detective.fs
+++ detective.fs
@@ -1,12 +1,23 @@
 17 ficl-vocabulary detective-voc
 also detective-voc definitions
 
+\ : in-area?  ( x/8 y/8 -- f )    0 24 WITHIN  SWAP  0 32 WITHIN  AND ;
 
-: ?exit     ( -- )  SCANCODE_Q pressed? IF  retro-40  THEN ;
+0 DEFINE-SFX jump.wav
 
+: xy>tilexy  ( x y -- x/8 y/8 )  8 /  SWAP  8 /  SWAP ;
+: tilexy>xy  ( x/8 y/8 -- x y )  8 *  SWAP  8 *  SWAP ;
 
-: in-area?  ( x/8 y/8 -- f )    0 24 WITHIN  SWAP  0 32 WITHIN  AND ;
-: tile>xy   ( x/8 y/8 -- x y )  8 *  SWAP  8 *  SWAP ;
+: place-dude  ( -- )  11  32 RND 24 RND  M! ;
+: reset       ( -- )  s" detective.map" load-map  place-dude ;
+: win         ( -- )  0 sfx  reset ;
+
+: check-dude  ( mx my -- f )  xy>tilexy  M@ 11 =  IF  win  THEN ;
+
+: update-mouse  ( -- )  MOUSEB @ IF  MOUSEX @  MOUSEY @ check-dude  THEN ;
+: ?exit         ( -- )  SCANCODE_Q pressed? IF  retro-40  THEN ;
+: <update>      ( -- )  update-mouse  ?exit ;
+
 
 \ only blit the map portion under the spotlight
 : partial-map  ( mx my -- )
@@ -14,17 +25,16 @@ also detective-voc definitions
   \ compensate for the mouse coordinates
   mx 16 <   mx 8 MOD NOT   OR  TO xc
   my 16 <   my 8 MOD NOT   OR  TO yc
-  mx 16 - 8 /  my 16 - 8 /   5 xc +  5 yc +  2OVER tile>xy  map*
+  mx 16 - 8 /  my 16 - 8 /   5 xc +  5 yc +  2OVER tilexy>xy  map*
 ;
 
 \ blit the spotlight, using white as the transparent color
 : spotlight  ( mx my --)  15 colorkey !   -24 UNDER+ -24 +   6 6  16 spr*   -1 colorkey ! ;
 
-
-: <init>    ( -- )  s" detective.spr" load-sprites  s" detective.map" load-map ;
-: <update>  ( -- )  ?exit ;
 : <draw>    ( -- )  0 cls  MOUSEX @ MOUSEY @ 2DUP  partial-map  spotlight ;
 
+
+: <init>    ( -- )  s" detective.spr" load-sprites  reset ;
 
 \ install the software
 PREVIOUS DEFINITIONS
